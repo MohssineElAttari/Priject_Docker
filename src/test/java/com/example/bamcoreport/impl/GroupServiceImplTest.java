12df634c35a607a5fba90eca683ec5293b73bb7a
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-02-23 11:52:42 +0100

    update test

--- src/test/java/com/example/bamcoreport/impl/GroupServiceImplTest.java
+++ src/test/java/com/example/bamcoreport/impl/GroupServiceImplTest.java
@@ -36,18 +36,18 @@ class GroupServiceImplTest {
 
     @Test
     void getAllGroup() {
-//        LocalDateTime now = LocalDateTime.now();
-//        Group group1 = new Group(1L,"group1","group1","group1","group1",new User(),now,now);
-//        Group group2 = new Group(2L,"group2","group2","group2","group2",new User(),now,now);
-//        Group group3 = new Group(3L,"group3","group3","group3","group3",new User(),now,now);
-//
-//        List<Group> groups = new ArrayList<>();
-//        groups.add(group1);
-//        groups.add(group2);
-//        groups.add(group3);
-//        doReturn(groups).when(groupRepository).findAll();
-////        Mockito.when(groupService.getAllGroup()).thenReturn(groups);
-////        assertNotNull(groupService.getAllGroup());
+        LocalDateTime now = LocalDateTime.now();
+        Group group1 = new Group(1L,"group1","group1","group1","group1",new User(),now,now);
+        Group group2 = new Group(2L,"group2","group2","group2","group2",new User(),now,now);
+        Group group3 = new Group(3L,"group3","group3","group3","group3",new User(),now,now);
+
+        List<Group> groups = new ArrayList<>();
+        groups.add(group1);
+        groups.add(group2);
+        groups.add(group3);
+        doReturn(groups).when(groupRepository).findAll();
+        Mockito.when(groupService.getAllGroup()).thenReturn(groups);
+        assertNotNull(groupService.getAllGroup());
 //        List<Group> groupList = groupService.getAllGroup();
 //        assertThat(groupList).isNotEmpty();
     }
